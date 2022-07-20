13b4f8e677f29db51d37da42ac1f0ea89e7078e8
Anvay Bhanap <anvay.bhanap@gmail.com>
2022-07-20 14:51:02 +0530

    added email based GET functionality

--- src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -31,11 +31,10 @@ public class EmployeeController {
     }
 
 //    get employee by id
-    @GetMapping("/employees/{id}")
-    public ResponseEntity<Employee> getEmployeeById(@PathVariable(value = "id") Long employeeId)
-            throws ResourceNotFoundException {
-        Employee employee = employeeRepository.findById(employeeId)
-                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+    @GetMapping("/employees/{emailId}")
+    public ResponseEntity<Employee> getEmployeeByEmailId(@PathVariable(value = "emailId") String employeeEmail) {
+        Employee employee = employeeRepository.findByEmailId(employeeEmail);
+//                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this email :: " + employeeEmail));
         return ResponseEntity.ok().body(employee);
     }
 
@@ -47,12 +46,11 @@ public class EmployeeController {
     }
 
 //    update employee
-    @PutMapping("/employees/{id}")
-    public ResponseEntity<Employee> updateEmployee(@PathVariable(value = "id") Long employeeId,
+    @PutMapping("/employees/{emailId}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable(value = "id") String employeeEmail,
                                                    @Validated @RequestBody Employee employeeDetails) throws ResourceNotFoundException {
-        Employee employee = employeeRepository.findById(employeeId)
-                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
-
+        Employee employee = employeeRepository.findByEmailId(employeeEmail);
+//                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this email :: " + employeeEmail));
         employee.setEmailId(employeeDetails.getEmailId());
         employee.setLastName(employeeDetails.getLastName());
         employee.setFirstName(employeeDetails.getFirstName());
@@ -61,12 +59,10 @@ public class EmployeeController {
     }
 
 //    delete employee
-    @DeleteMapping("/employees/{id}")
-    public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long employeeId)
-            throws ResourceNotFoundException {
-        Employee employee = employeeRepository.findById(employeeId)
-                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
-
+    @DeleteMapping("/employees/{emailId}")
+    public Map<String, Boolean> deleteEmployee(@PathVariable(value = "emailId") String employeeEmail) {
+        Employee employee = employeeRepository.findByEmailId(employeeEmail);
+//                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this email :: " + employeeEmail));
         employeeRepository.delete(employee);
         Map<String, Boolean> response = new HashMap<>();
         response.put("deleted", Boolean.TRUE);
